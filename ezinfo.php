427f8c4b6296232439e00a80f435ae3f31922673
Brookins Consulting <info@brookinsconsulting.com>
2012-03-25 17:16:22 -0500

    Added: Initial import of bcsimplejscsscompresserdisablesettings extension

---
+++ ezinfo.php
@@ -0,0 +1,38 @@
+<?php
+//
+// ## BEGIN COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+// SOFTWARE NAME: BC Simple JS/CSS Compresser Disable Settings
+// SOFTWARE RELEASE: 0.1.0
+// COPYRIGHT NOTICE: Copyright (C) 1999-2012 eZ Systems AS, 1999-2012 Brookins Consulting
+// SOFTWARE LICENSE: GNU General Public License v2.0 (or later)
+// NOTICE: >
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the GNU General Public Licenseas
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of version 2.0 of the GNU General
+//   Public License along with this program; if not, write to the Free
+//   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+//   MA 02110-1301, USA.
+//
+//
+// ## END COPYRIGHT, LICENSE AND WARRANTY NOTICE ##
+//
+
+class bcsimplejscsscompresserdisablesettingsInfo
+{
+    static function info()
+    {
+        return array( 'Name' => 'BC Simple JS/CSS Compresser Disable : Settings',
+                      'Version' => '0.1.0',
+                      'Copyright' => 'Copyright (C) 1999-2012 eZ Systems AS, 1999-2012 Brookins Consulting',
+                      'License' => 'GNU General Public License'
+                     );
+    }
+}
+?>
